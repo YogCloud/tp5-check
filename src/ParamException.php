6c8b6cd8aba14503802819afd68cbab6403e1696
h6play <61290455+h6play@users.noreply.github.com>
2020-07-05 09:57:22 +0800

    Update ParamException.php

--- src/ParamException.php
+++ src/ParamException.php
@@ -4,7 +4,13 @@
 namespace TpCheck;
 
 
-class ParamException extends ServerException
+use Throwable;
+
+class ParamException extends \Exception
 {
 
+    public function __construct($message = "请求参数验证错误", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
